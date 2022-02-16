203305107bb73b63b6c8872367d8e9bc51102892
Md. Razun Mia <razun.neub.cse@gmail.com>
2022-02-16 17:31:10 +0600

    Logger functionalities have been added in linkedIn login  function.

--- routes/linkedinRoute.js
+++ routes/linkedinRoute.js
@@ -7,6 +7,8 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const redisClient  = require('../db/redis')
 const  userCollection = database.collection("user")
+const logger = require('../logger/logger')
+
 
 router.get('/linkedin',  passport.authenticate('linkedin', {
     scope: ['r_emailaddress', 'r_liteprofile'],
@@ -27,25 +29,35 @@ router.get('/linkedin/callback',
 
     }
     userCollection.findOne({userEmail: userEmail}, (err, result)=>{
-      if(err) return res.status(500).send({errorMessage: "Something went wrong"})
+      if(err){
+        logger.log({level: 'error', message: 'Internal error for user login by using linkedin.'})
+        return res.status(500).send({errorMessage: "Something went wrong"})
+      } 
       if(result==null){
         const authToken = jwt.sign({userEmail: userEmail},process.env.TOKEN_SECRET)
         const refreshToken = jwt.sign({userEmail: userEmail}, process.env.REFRESH_TOKEN_SECRET)
         userCollection.insertOne(linkedInUser)
         redisClient.set(userEmail, refreshToken,{ EX: 365*24*60*60} , (err, reply)=>{
-          if(err) return res.status5(500).send({errorMessage:"Something went wrong."})
+          if(err){
+            logger.log({level: 'error', message: 'Internal error for user login by using linkedin.'})
+            return res.status(500).send({errorMessage: "Something went wrong"})
+          }
       })
-        res.status(200).send({linkedInSuccessMessageAndInserted:"User has been logged in successfully.",authToken: authToken, refreshToken: refreshToken})
+      logger.log({level: 'info', message: 'User has been logged in successfully by using linkedIn.'})
+      res.status(200).send({linkedInSuccessMessageAndInserted:"User has been logged in successfully.",authToken: authToken, refreshToken: refreshToken})
       }else{
           const authToken = jwt.sign({userEmail: result.userEmail},process.env.TOKEN_SECRET)
           const refreshToken = jwt.sign({userEmail: userEmail}, process.env.REFRESH_TOKEN_SECRET)
           redisClient.set(userEmail, refreshToken,{ EX: 365*24*60*60} , (err, reply)=>{
-            if(err) return res.status(500).send({errorMessage:"Something went wrong."})
+            if(err){
+              logger.log({level: 'error', message: 'Internal error for user login by using linkedin.'})
+              return res.status(500).send({errorMessage:"Something went wrong."})
+            } 
         })
-          res.status(200).send({linkedInExistingSuccessMessage: "User Already exist.", authToken: authToken, refreshToken: refreshToken})
+        logger.log({level: 'error', message: 'User Already exist for linkedin account.'})
+        res.status(200).send({linkedInExistingSuccessMessage: "User Already exist.", authToken: authToken, refreshToken: refreshToken})
       }
-  }
-  )
+  })
   }
   );
 
