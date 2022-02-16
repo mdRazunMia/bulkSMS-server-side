b4335a94969519bb0ddb2cc15823b0adadf1c0f2
Md. Razun Mia <razun.neub.cse@gmail.com>
2022-02-16 16:12:48 +0600

    Logger functionalities have been added in campaign function.

--- controllers/campaign.js
+++ controllers/campaign.js
@@ -1,22 +1,30 @@
 const database = require('../db/database')
-
+const logger = require('../logger/logger')
 const  campaignCollection = database.collection("campaign_details")
 
 const createCampaign = (req, res)=>{
     const campaignInformation = req.body
     let verified = false
     campaignCollection.insertOne(campaignInformation, (err, response)=>{
-        if(err) return res.status(500).send({errorMessage: "Something went wrong"})
+        if(err){
+            logger.log({level: 'error', message: 'Internal error for create campaign in database'})
+            return res.status(500).send({errorMessage: "Something went wrong"})
+        } 
         if(response.acknowledged){
             verified = true
         }
     })
+    logger.log({level: 'info', message: 'Campaign has been created successfully.'})
    res.status(201).send({ message: "Campaign has been created successfully."})
 }
 
 const showAllCampaign = (req, res)=>{
     campaignCollection.find({}).toArray((err, result)=>{
-        if(err) return res.status(500).send({errorMessage: "Something went wrong"})
+        if(err) {
+            logger.log({level: 'error', message: 'Internal error for create campaign in database'})
+            return res.status(500).send({errorMessage: "Something went wrong"})
+        }
+        logger.log({level: 'info', message: 'Send all the campaign information.'})
         res.status(200).send({
             campaigns: result
         })
@@ -27,7 +35,11 @@ const deleteCampaign = (req, res)=>{
     const campaignId = req.params.campaignId
     var deletedCampaignId = { _id: ObjectId(campaignId) };
     campaignCollection.deleteOne(deletedCampaignId,(err,data)=>{
-        if(err) return res.status(500).send({errorMessage: "Something went wrong"})
+        if(err) {
+            logger.log({level: 'error', message: 'Internal error for create campaign in database'})
+            return res.status(500).send({errorMessage: "Something went wrong"})
+        }
+        logger.log({level: 'info', message: 'Campaign has been created successfully.'})
         res.status(200).send({
             message: `Campaign id ${campaignId} has been deleted successfully`
         })
