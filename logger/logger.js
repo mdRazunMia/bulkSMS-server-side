743a73dbb8ebd3a7c5985c191d32122d682c465c
Md. Razun Mia <razun.neub.cse@gmail.com>
2022-02-22 15:12:43 +0600

    Modify logic in logger file.

--- logger/logger.js
+++ logger/logger.js
@@ -3,24 +3,58 @@ const { combine, timestamp, printf } = format;
 require('winston-mongodb')
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rv6z4.mongodb.net/bulkSMS?retryWrites=true&w=majority`;
+
 let transport_array = []
 
 if(process.env.LOGGER_CONSOLE_ONLY==true){
-  const trans_console = new transports.Console()
-  transport_array.push(trans_console)
+  transport_array = [
+    new transports.Console()
+  ]
+
 }else if(process.env.LOGGER_FILE_ONLY==true){
-  const trans_file = new transports.File({ filename: 'logFile.log'})
-  transport_array.push(trans_file)
-}else if(process.env.LOGGER_FILE_ONLY==true){
-  const trans_db = new transports.MongoDB({
-    db: uri,
-  //   level: 'error',
-    collection: 'error-log',
-    options: {
+  transport_array = [
+    new transports.File({ filename: 'logFile.log'})
+  ]
+}else if(process.env.LOGGER_DB_ONLY==true){
+transport_array = [
+  new transports.MongoDB({
+      db: uri,
+     //level: 'error',
+      collection: 'error-log',
+      options: {
         useUnifiedTopology: true
-    }
-})
-  transport_array.push(trans_db)
+      }
+  })
+]
+}else if(process.env.LOGGER_CONSOLE_ONLY==true && process.env.LOGGER_FILE_ONLY==true){
+  transport_array = [
+    new transports.Console(),
+    new transports.File({ filename: 'logFile.log'})
+  ]
+}else if(process.env.LOGGER_CONSOLE_ONLY==true && process.env.LOGGER_DB_ONLY==true){
+  transport_array = [
+    new transports.Console(),
+    new transports.MongoDB({
+      db: uri,
+    //   level: 'error',
+      collection: 'error-log',
+      options: {
+          useUnifiedTopology: true
+      }
+  })
+  ]
+}else if(process.env.LOGGER_FILE_ONLY==true && process.env.LOGGER_DB_ONLY==true){
+  transport_array = [
+    new transports.File({ filename: 'logFile.log'}),
+    new transports.MongoDB({
+      db: uri,
+    //   level: 'error',
+      collection: 'error-log',
+      options: {
+          useUnifiedTopology: true
+      }
+    })
+  ]
 }else{
   transport_array = [
     new transports.Console(),
@@ -41,7 +75,6 @@ const myFormat = printf(({ level, message, timestamp }) => {
 });
 
 
-
 const logger = createLogger({
   format: combine(
     // format.colorize(),
@@ -50,18 +83,6 @@ const logger = createLogger({
     myFormat
   ),
   transports: transport_array
-  // transports: [
-  //     new transports.Console(),
-  //     new transports.File({ filename: 'logFile.log'}),
-  //     new transports.MongoDB({
-  //         db: uri,
-  //       //   level: 'error',
-  //         collection: 'error-log',
-  //         options: {
-  //             useUnifiedTopology: true
-  //         }
-  //     })
-  // ]
 });
 
 module.exports = logger
