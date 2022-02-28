9ca784ed1035298bc3e6fcc8ccf872fa6d6181e0
Md. Razun Mia <razun.neub.cse@gmail.com>
2022-02-28 18:24:15 +0600

    Redis client has been added in linkedIn login function.

--- db/redis.js
+++ db/redis.js
@@ -37,34 +37,3 @@ module.exports = (function () {
     redisConnectAndGetTheInstance,
   };
 })();
-
-// --------------------------------------------------------
-
-// const client = redis.createClient({
-//   port: process.env.REDIS_PORT,
-//   host: `${process.env.REDIS_HOST}`,
-// });
-
-// client.connect();
-// // console.log(client);
-// client.on("connect", () => {
-//   console.log("Successfully connected to redis database.");
-// });
-
-// client.on("ready", () => {
-//   console.log("Client connected to redis database and ready to use...");
-// });
-
-// client.on("error", (error) => {
-//   console.log(error.message);
-// });
-
-// client.on("end", () => {
-//   console.log("Client disconnected from redis.");
-// });
-
-// process.on("SIGINT", () => {
-//   client.quit();
-// });
-// //
-// module.exports = client;
